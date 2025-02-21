debc196549fd6002b01bcfa72744469737770304
Areeba Bano <161935706+areebabano@users.noreply.github.com>
2025-02-22 01:26:37 +0500

    some minor changes

--- views/challenge.py
+++ views/challenge.py
@@ -83,8 +83,6 @@ display_name()
 # initialize the amount variable
 amount = 10000
 
-# check that You are eligible to
-#  purchase Dsa Self Paced or not
 if amount > 2999:
     print("You are eligible to purchase Dsa Self Paced")
 """,
